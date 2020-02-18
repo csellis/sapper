ebc0a51a67b7456551af7af2d03030486d4eba76
Chris Ellis <ellis.cs@gmail.com>
2020-02-18 06:08:12 -0500

    Style error page

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,40 +1,46 @@
 <script>
-	export let status;
-	export let error;
+  export let status;
+  export let error;
 
-	const dev = process.env.NODE_ENV === 'development';
+  const dev = process.env.NODE_ENV === "development";
 </script>
 
 <style>
-	h1, p {
-		margin: 0 auto;
-	}
+  h1,
+  p {
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  p {
+    margin: 1em auto;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>{status}</title>
+  <title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
-
-<p>{error.message}</p>
+<section
+  class="container mx-auto sm:w-3/4 lg:w-1/2 sm:px-4 sm:py-8 m-8 mb-24 bg-white
+  shadow text-gray-800 max-w-3xl">
+  <div class="flex flex-col items-center justify-center">
+    <h2 class="font-semibold text-3xl pt-12 pb-8">{status}</h2>
+    <h3 class="text-xl pb-12">{error.message}</h3>
+  </div>
+</section>
 
 {#if dev && error.stack}
-	<pre>{error.stack}</pre>
+  <pre>{error.stack}</pre>
 {/if}
